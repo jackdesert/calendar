716068e158010a2dff146e29f5db3668cb533041
Jack Desert <jackdesert@gmail.com>
2016-06-30 21:22:17 -0500

    Website displays with carousel

--- src/api/handler/calendar.go
+++ src/api/handler/calendar.go
@@ -5,14 +5,28 @@ import (
 	//"github.com/davecgh/go-spew/spew"
 	"github.com/jackdesert/calendar/src/event"
 	"html/template"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func Calendar(w http.ResponseWriter, r *http.Request) {
 
 	carouselInStruct := event.CarouselInStruct()
 	//spew.Dump(carousel)
+
+	funcMap := template.FuncMap{
+		// The name "title" is what the function will be called in the template text.
+		"title": strings.Title,
+	}
+	log.Println(funcMap)
+
+	//	t, _ := template.New("abalone").Funcs(funcMap).ParseFiles("index.html")
 	t, _ := template.ParseFiles("index.html")
+	//t, _ := template.New("abalone").ParseFiles("index.html")
+	log.Println("Template Defined")
+	//t.Funcs(funcMap)
+	//log.Println("Funcs Added")
 	//defer return500IfError(w)
 	t.Execute(w, carouselInStruct)
 	//fmt.Fprintf(w, event.render())
